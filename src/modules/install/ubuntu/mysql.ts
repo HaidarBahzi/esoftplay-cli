897443d255fd346a287388195e394fe35940037b
HaidarBahzi <biglamp.gang@gmail.com>
2024-10-17 13:21:24 +0700

    package error fix

--- src/modules/install/ubuntu/mysql.ts
+++ src/modules/install/ubuntu/mysql.ts
@@ -8,11 +8,23 @@ export default async function InstallMysqlUbuntu() {
     execSync("sudo apt -y remove mariadb* mysql* libc6-dev > /dev/null 2>&1");
 
     execSync(
-      "sudo apt -y install apt-utils libsuma1 libatomic1 libaio1 libncurses6 libmecab2 psmisc > /dev/null 2>&1"
+      "sudo apt -y install apt-utils libsuma1 libnuma1 libatomic1 libncurses6 libmecab2 psmisc > /dev/null 2>&1"
     );
 
     execSync(
-      "wget https://cdn.mysql.com/archives/mysql-5.7/mysql-server_5.7.42-1ubuntu18.04_amd64.deb-bundle.tar"
+      "wget http://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libtinfo5_6.4-2_amd64.deb > /dev/null 2>&1"
+    );
+
+    execSync(
+      "wget http://archive.ubuntu.com/ubuntu/pool/main/liba/libaio/libaio1_0.3.113-5_amd64.deb > /dev/null 2>&1"
+    );
+
+    execSync(
+      "sudo apt install ./libaio1_0.3.113-5_amd64.deb ./libtinfo5_6.4-2_amd64.deb > /dev/null 2>&1"
+    );
+
+    execSync(
+      "wget https://cdn.mysql.com/archives/mysql-5.7/mysql-server_5.7.42-1ubuntu18.04_amd64.deb-bundle.tar > /dev/null 2>&1"
     );
 
     mkdirSync("temp-mysql");
@@ -22,6 +34,10 @@ export default async function InstallMysqlUbuntu() {
     );
 
     unlinkSync("mysql-server_5.7.42-1ubuntu18.04_amd64.deb-bundle.tar");
+
+    unlinkSync("libaio1_0.3.113-5_amd64.deb");
+
+    unlinkSync("libtinfo5_6.4-2_amd64.deb");
 
     process.chdir("temp-mysql");
 
